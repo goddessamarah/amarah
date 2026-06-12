c65c1a16637858ce4f389af37eccbc22c2366bea
Amarah <littleevie@disroot.org>
2026-06-11 21:36:07 -0500

    Start working on dollicons page

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
   eleventyConfig.addPassthroughCopy({ "img/favicon": "/" });
+  eleventyConfig.addPassthroughCopy({ "img/other": "/" });
+  eleventyConfig.addPassthroughCopy({ font: "/" });
 
   return {
     dir: {
